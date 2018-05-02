54889cef9a91a070ffb3263e5333a8060df06fdc
toburencale@gmail.com <35843725+toburencale@users.noreply.github.com>
2018-05-02 12:13:22 -0500

    added movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿//Cale Toburen
+//Date
+//Project
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
