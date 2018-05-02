d818ed55e868174a3fdb4e170a1e37e754b8d6ad
toburencale@gmail.com <35843725+toburencale@users.noreply.github.com>
2018-05-02 12:14:24 -0500

    shooting

---
+++ Assets/Shooting.cs
@@ -0,0 +1,20 @@
+﻿//Cale Toburen
+//Date
+//Project
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+	
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
